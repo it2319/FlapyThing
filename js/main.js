99361e5528245e7f17006405c32b489fe66f4d09
it2319 <it2319@sspu-opava.cz>
2025-01-18 17:35:29 +0100

    img,imgLoad,IMAGESobject

--- js/main.js
+++ js/main.js
@@ -6,6 +6,20 @@ canvas.height = window.innerHeight;
 
 const ctx = canvas.getContext("2d");
 
+const IMAGES = {
+    submarine:null,
+    mine:null,
+    background:null,
+};
+function loadImages(){
+    loadImages();
+}
+function loadImages(){
+    for (let i = 0; i < Object.keys(IMAGES).length; i++){
+        img_name = Object.keys(IMAGES)[i];
+        IMAGES[img_name] = loadImage(`../img/${img_name}.png`);
+    }
+    }
 function drawPlayer(){
     ctx.fillStyle = `rgb(82,5,10)`;
     ctx.fillRect(20, 20, 100, 100);
