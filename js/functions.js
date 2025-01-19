bfe7b5a9f04d2adb86804d7c5a99b2ce40a6bfad
ZZ <it2313@sspu-opava.cz>
2025-01-19 12:21:32 +0100

    Project reformat

---
+++ js/functions.js
@@ -0,0 +1,3 @@
+function generateMines() {}
+
+function checkCollisions() {}
--- js/main.js
+++ js/main.js
@@ -1,67 +1,26 @@
-
-const canvas = document.getElementById("game");
-
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-const ctx = canvas.getContext("2d");
-
-const IMAGES = {
-    submarine:null,
-    mine:null,
-    background:null,
-};
-function preload(){
-    loadImages();
+// !!! www.remove.bg/
+function preload() {
+  // load images
+  for (let i = 0; i < Object.keys(IMAGES).length; i++) {
+    img_name = Object.keys(IMAGES)[i];
+    IMAGES[img_name] = loadImage(`../img/${img_name}.png`);
+  }
 }
-function loadImages(){
-    for (let i = 0; i < Object.keys(IMAGES).length; i++){
-        img_name = Object.keys(IMAGES)[i];
-        IMAGES[img_name] = loadImage(`../img/${img_name}.png`);
-    }
-    }
 
-    /*let mines = []
+function setup() {
+  // P5 canvas
+  createCanvas(canvasWidth, canvasHeight);
 
-    mines.push(new Mine(100, 100, 50, 50))
+  // Mines
+  // xx
 
-class Mine{
-    constructor(x, y, width, height, mine){
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
-    this.mine = IMAGES.mine;
-    }
-    someMine(){
-        this.x = Math.random() * 100;
-        this.y = Math.random() * 100;
-        this.width = Math.random() * 100;
-        this.height = Math.random() * 100;
-        draw();
-    }
-    draw(){
-        ctx.drawImage(IMAGES.mine, this.x, this.y, this.width, this.height);
-    }
+  // Submarine
+  submarine = new Submarine(80, canvasHeight / 2, 160, 80);
 }
-    */
-function setup(){
-class Submarine{
-    constructor(x,y,height,width,player){
-        this.x = x;
-        this.y = y;
-        this.height = height;
-        this.width = width;
-        this.player_img = player_img;
-}
-    displaySubmarine(){
-        ctx.drawImage(this.x, this.y, this.width, this.height,IMAGES.submarine);
-    }
-}
-}
-function draw(){
 
-    let submarine1 = new Submarine(100, 100, 50, 50, `IMAGES.submarine`);
+function draw() {
+  imageMode(CORNER);
+  image(IMAGES["background"], 0, 0, canvasWidth, canvasHeight);
 
-    submarine1.displaySubmarine();
+  submarine.draw();
 }
